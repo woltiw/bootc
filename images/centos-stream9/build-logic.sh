a6c58d04e8fb9757d66d6ed539929eb4b16bbbce
woltiw <wvandoorn95@gmail.com>
2026-02-09 18:53:46 +0100

    chore: drop centos9

--- images/centos-stream9/build-logic.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-BASE_IMAGE=quay.io/centos-bootc/centos-bootc
-BASE_TAG=stream9
-
-BUILD_ARGS+=(
-  --build-arg="BASE_IMAGE=${BASE_IMAGE}"
-  --build-arg="BASE_TAG=${BASE_TAG}"
-)
